c403f9fce729f713d1a71556dad52aa4e5f4564d
Sophia Law <sophialaw927@gmail.com>
2020-04-22 13:11:31 -0500

    added an entry to the array

---
+++ src/scripts/journal.js
@@ -0,0 +1,10 @@
+const journalEntry = {
+   date: "2020-04-17",
+   concept: "functions",
+   content: "reviewed functions, which are essentially recipes that create tasks"
+    
+}
+
+const entries = []
+
+entries.push(journalEntry);
